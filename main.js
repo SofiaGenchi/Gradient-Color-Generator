8439ebe681bdef1aa342eb6196b3af3e1d1ab45a
Sofia Genchi <sofigen16@gmail.com>
2024-01-09 12:23:51 -0300

    textarea

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const colorInputs = document.querySelectorAll(".colors input");
 const gradientBox = document.querySelector(".gradient-box");
 
 const selectMenu = document.querySelector(".select-box select");
+const textarea = document.querySelector("textarea");
 
 const generateGradient = () => {
     // console.log("Color update...")
     const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
     // console.log(gradient)
     gradientBox.style.background = gradient;
+    textarea.value = `background: ${gradient}`;
 }
 
 colorInputs.forEach(input => {
