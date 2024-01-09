2d7f02cff7df272af640991422c15fa1c7510303
Sofia Genchi <sofigen16@gmail.com>
2024-01-09 12:22:03 -0300

    direction

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const colorInputs = document.querySelectorAll(".colors input");
 const gradientBox = document.querySelector(".gradient-box");
 
+const selectMenu = document.querySelector(".select-box select");
+
 const generateGradient = () => {
     // console.log("Color update...")
-    const gradient = `linear-gradient(to top left, ${colorInputs[0].value}, ${colorInputs[1].value})`;
+    const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
     // console.log(gradient)
     gradientBox.style.background = gradient;
 }
 
 colorInputs.forEach(input => {
     input.addEventListener("input", generateGradient);
-})
+});
+
+selectMenu.addEventListener("change", generateGradient);
