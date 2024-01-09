47199144b2abeacb76fd616491099bf28ee1bc82
Sofia Genchi <sofigen16@gmail.com>
2024-01-09 12:32:53 -0300

    refresh btn

--- main.js
+++ main.js
@@ -3,8 +3,19 @@ const gradientBox = document.querySelector(".gradient-box");
 
 const selectMenu = document.querySelector(".select-box select");
 const textarea = document.querySelector("textarea");
+const refreshBtn = document.querySelector(".refresh");
 
-const generateGradient = () => {
+const getRandomColor = () => {
+    const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return `#${randomHex}`;
+}
+
+const generateGradient = (isRandom) => {
+    if (isRandom) {
+        // console.log(getRandomColor())
+        colorInputs[0].value = getRandomColor();
+        colorInputs[1].value = getRandomColor();
+    }
     // console.log("Color update...")
     const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
     // console.log(gradient)
@@ -13,7 +24,8 @@ const generateGradient = () => {
 }
 
 colorInputs.forEach(input => {
-    input.addEventListener("input", generateGradient);
+    input.addEventListener("input", () => generateGradient(false));
 });
 
-selectMenu.addEventListener("change", generateGradient);
+selectMenu.addEventListener("change", () => generateGradient(false));
+refreshBtn.addEventListener("click", () => generateGradient(true))
